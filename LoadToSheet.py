124b21867011a69e4248e7d2f2a1b2377a8eb19a
Ben Charbonneau <benjamincharbonneau@icloud.com>
2024-09-11 23:55:46 -0400

    Little tweaks to comments

--- LoadToSheet.py
+++ LoadToSheet.py
@@ -5,21 +5,22 @@ attendanceForm = '9_5 Lunch BETA Attendance Form (Responses) - Form Responses 1.
 
 #opens csv and extracts emails column
 attForm = (pd.read_csv(attendanceForm))
-emails = attForm['Email Address']
+emails = attForm['Email Address'] #change this variable to change what column is pulled from
 
-#adds first 10 char of email (480s) to list
+#adds first 10 char of email (480s) to list + initialized with officers 480s
 att = ['4804315070', '4806003682', '4804169889', '4804168512', '4804170072', '4806894848', '4804169951', '4804209159', '4804189176', '4804174096', '4804205750']
 for i, num in enumerate(emails):
     if (emails.isna()[i] == False):
         att.append(num.strip()[0:10])
 att = list(set(att))
+
+
 #attendance csv file goes here ↓↓↓↓↓↓ <- if updated for new members needs to be re-downloaded
 betaLog = 'Beta Attendance 2024-2025 - Sheet1.csv'
 
 #opens csv and extracts student nums column
 log = (pd.read_csv(betaLog))
-stuNums = log['Student Number']
-
+stuNums = log['Student Number'] #change this variable to change what column is checked
 
 #checks each student num to see if recorded in form
 for p in stuNums:
